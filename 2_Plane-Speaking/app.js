4503e2727d9a0a11e0626eb04ddc986d05d1adfe
Mirman-comarley2016 <corey@mirman.org>
2015-10-06 08:52:56 -0700

    Completed plane speaking

--- 2_Plane-Speaking/app.js
+++ 2_Plane-Speaking/app.js
@@ -1,12 +1,12 @@
-var xPlanes = [
-  "X-1",
-  "X-3",
-  "X-15",
-  "X-29",
-  "X-31",
-  "X-37",
-  "X-45"
-];
+var xJSON = {
+  "X-1": "Broke Mach 1",
+  "X-3": "Butt ugly",
+  "X-15": "Reached Hypersonic level",
+  "X-29": "Forward-Swept-Wing test",
+  "X-31": "Tested ESTOL",
+  "X-37": "Reusable",
+  "X-45": "Unmanned aircraft"
+}
 
 // Now that we have our array of planes, we need to add elements to our DOM
 // for each one.
@@ -15,5 +15,6 @@ var e = document.getElementById('planes-list'); //This line is unfinished! Use a
 for(var i=0; i<xPlanes.length; i++) {
   var xPlane = xPlanes[i];
   e.innerHTML += "<li>" + xPlane + "</li>"; //What does "i" refer to?
+  //e.innerHTML +=
   // Do some things to add <li> elements to our empty list.
 }

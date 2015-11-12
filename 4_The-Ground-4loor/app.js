6d9e82bc6738c1352c4606e35cb8ceb418352476
Mirman-comarley2016 <corey@mirman.org>
2015-11-12 15:06:43 -0800

    Finished Foundations

--- 4_The-Ground-4loor/app.js
+++ 4_The-Ground-4loor/app.js
@@ -1,11 +1,25 @@
 $(document).ready(function(){
   $("#contact-form").submit(function(event){
     event.preventDefault();
-    //Use jQuery to collect the values from the fields and create a
-    //JSON object containing them. Then, log the result to the console
 
-    // Secondary Objective: Use the AJAX methods built into jQuery to send
-    // The form data to http://x.mirman.org:9999. Write a callback to handle
-    // the response.
+    var x = $("#firstname").val();
+    var y = $("#lastname").val();
+    var z = $("#email").val();
+    var a = $("#message").val();
+
+    var newObj = {
+      firstname: x,
+      lastname: y,
+      email: z,
+      message: a
+    }
+    $.post
+    console.log(event);
   })
 });
+//Use jQuery to collect the values from the fields and create a
+//JSON object containing them. Then, log the result to the console
+
+// Secondary Objective: Use the AJAX methods built into jQuery to send
+// The form data to http://x.mirman.org:9999. Write a callback to handle
+// the response.

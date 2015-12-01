4d67de6411f7c20822db9716e752315218c5154c
Mirman-comarley2016 <corey@mirman.org>
2015-12-01 10:33:44 -0800

    Almost done with ground 4loor and fizzbuzz

--- 5_Into-the-Fire/js/feedback.js
+++ 5_Into-the-Fire/js/feedback.js
@@ -1,4 +1,4 @@
-var ref = new Firebase(""); //Use your app's Firebase URL
+var ref = new Firebase("https://blazing-heat-1916.firebaseio.com/"); //Use your app's Firebase URL
 
 // Add some event handlers for FB events
 
@@ -10,3 +10,13 @@ $(document).ready(function(){
     // Go grab your info and do something with it.
   });
 });
+
+function(snapshot)  {
+  var data = spanshot.val();
+  for(var c in data) {
+    var comment = data[c];
+    var newLI = $(document.createElement("li"));
+    newli.append(data.name);
+    $("#comments").append(newLi)
+  }
+}

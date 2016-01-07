574915817c3ac2044c65193d958a348cdce06770
Mirman-comarley2016 <corey@mirman.org>
2016-01-07 14:45:07 -0800

    Working on 5ire

--- 5_Into-the-Fire/js/feedback.js
+++ 5_Into-the-Fire/js/feedback.js
@@ -3,39 +3,38 @@ var ref = new Firebase("https://blazing-heat-1916.firebaseio.com/"); //Use your 
 // Add some event handlers for FB events
 $(document).ready(function(){
   console.log("Document loaded");
-  $("#add-form").submit(function(event){
+  $("#contact-form").submit(function(event){
     event.preventDefault();
-    console.log("Form submitted!");
-    // Go grab your info and do something with it.
+    console.log("SUBMIT");
+    var x = $("#firstname").val();
+    var y = $("#lastname").val();
+    var z = $("#email").val();
+    var a = $("#message").val();
+
+    var results = {
+      firstname: x,
+      lastname: y,
+      email: z,
+      message: a
+    }
+    ref.push(results);
   });
+
 });
-var x = $("#firstname").val();
-var y = $("#lastname").val();
-var z = $("#email").val();
-var a = $("#message").val();
+
 // Attach an asynchronous callback to read the data at our posts reference
 ref.on("value", function(snapshot)  {
-  var data = spanshot.val();
+  var data = snapshot.val();
+  console.log(data);
   for(var c in data) {
     var comment = data[c];
-    var newLI = $(document.createElement("li"));
-    newli.append(data.name);
-    $("#comments").append(newLi)
+    var newLi = $(document.createElement("li"));
+    newLi.append(data.name);
+    $("#contact-form").append(newLi)
     console.log(snapshot.val());
   }
 }), function (errorObject) {
   console.log("The read failed: " + errorObject.code);
 };
 
-var results = {
-  firstname: x,
-  lastname: y,
-  email: z,
-  message: a
-}
-
-$("snapshot").data("results");
-$.post("https://blazing-heat-1916.firebaseio.com/" function(snapshot)  (<li> "firstname" + "lastname" + "email" + "message")
-
-  //$.append(newLi)
-  //$.append(newLi)(<li> )
+newLi.append(<li> "#firstname" </li> + <li> "#lastname" </li> + <li> "#email" </li> + <li> "message" </li>)
